1c1d29b6dd69819b5e6aa566f61b90cd98f4433f
Brian Rowe <browe@pivotal.io>
2018-04-26 15:29:57 -0700

    improving message layout

--- geode-protobuf/src/main/java/org/apache/geode/protocol/serialization/ProtobufTypedStructSerializer.java
+++ geode-protobuf/src/main/java/org/apache/geode/protocol/serialization/ProtobufTypedStructSerializer.java
@@ -14,21 +14,26 @@
  */
 package org.apache.geode.protocol.serialization;
 
+import static org.apache.geode.protocol.serialization.ProtobufStructSerializer.PROTOBUF_STRUCT;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.NullValue;
+import com.google.protobuf.UnsafeByteOperations;
 
 import org.apache.geode.cache.Cache;
-import org.apache.geode.internal.protocol.protobuf.v1.Struct;
+import org.apache.geode.internal.protocol.protobuf.v1.BasicTypes;
+import org.apache.geode.internal.protocol.protobuf.v1.ProtobufSerializationService;
 import org.apache.geode.internal.protocol.protobuf.v1.TypedStruct;
-import org.apache.geode.internal.protocol.protobuf.v1.Value;
 import org.apache.geode.pdx.PdxInstance;
+import org.apache.geode.pdx.PdxInstanceFactory;
 
 public class ProtobufTypedStructSerializer implements ValueSerializer {
-  private ProtobufStructSerializer structSerializer = new ProtobufStructSerializer();
+  private Cache cache;
 
   private HashMap<Integer, List<String>> serverTypeMap = new HashMap<>();
   int nextServerIndex = 1;
@@ -44,52 +49,101 @@ public class ProtobufTypedStructSerializer implements ValueSerializer {
     return index;
   }
 
+
   @Override
   public ByteString serialize(Object object) throws IOException {
+    return serializeStruct(object).toByteString();
+  }
+
+  TypedStruct serializeStruct(Object object) {
     PdxInstance pdxInstance = (PdxInstance) object;
 
-    TypedStruct.Builder typedStructBuilder = TypedStruct.newBuilder();
+    TypedStruct.Builder structBuilder = TypedStruct.newBuilder();
     for (String fieldName : pdxInstance.getFieldNames()) {
       Object value = pdxInstance.getField(fieldName);
-      Value serialized = structSerializer.serializeValue(value);
-      typedStructBuilder.addFields(serialized);
+      BasicTypes.EncodedValue serialized = serializeValue(value);
+      structBuilder.addFields(serialized);
     }
 
-    return typedStructBuilder.build().toByteString();
+    return structBuilder.build();
+  }
+
+  BasicTypes.EncodedValue serializeValue(Object value) {
+    BasicTypes.EncodedValue.Builder builder = BasicTypes.EncodedValue.newBuilder();
+    if (value instanceof String) {
+      builder.setStringResult((String) value).build();
+    } else if (value instanceof Boolean) {
+      builder.setBooleanResult((Boolean) value).build();
+    } else if (value instanceof Integer) {
+      builder.setIntResult((Integer) value).build();
+    } else if (value instanceof Byte) {
+      builder.setByteResult((Byte) value).build();
+    } else if (value instanceof Long) {
+      builder.setLongResult((Long) value).build();
+    } else if (value instanceof Double) {
+      builder.setDoubleResult((Double) value).build();
+    } else if (value instanceof byte[]) {
+      builder.setBinaryResult(UnsafeByteOperations.unsafeWrap((byte[]) value)).build();
+    } else if (value instanceof PdxInstance) {
+      builder.setCustomObjectResult(serializeStruct(value).toByteString());
+    } else if (value instanceof List) {
+      throw new IllegalStateException("We don't do lists!");
+    } else if (value == null) {
+      builder.setNullResult(NullValue.NULL_VALUE).build();
+    } else {
+      throw new IllegalStateException(
+          "Don't know how to translate object of type " + value.getClass() + ": " + value);
+    }
+    return builder.build();
   }
 
   @Override
   public Object deserialize(ByteString bytes) throws IOException, ClassNotFoundException {
     TypedStruct struct = TypedStruct.parseFrom(bytes);
-    return structSerializer.deserialize(typedStructToStruct(struct));
+    List<String> fieldNames = serverTypeMap.get(struct.getTypeId());
+    if (fieldNames.size() != struct.getFieldsCount()) {
+      throw new IOException("Incorrect number of fields for encoded type");
+    }
+
+    PdxInstanceFactory pdxInstanceFactory = cache.createPdxInstanceFactory(PROTOBUF_STRUCT);
+
+    Iterator<String> keys = fieldNames.iterator();
+    Iterator<BasicTypes.EncodedValue> values = struct.getFieldsList().iterator();
+    while (keys.hasNext() && values.hasNext()) {
+      final String fieldName = keys.next();
+      final Object value = new ProtobufSerializationService(this).decode(values.next());
+
+      if (value instanceof String) {
+        pdxInstanceFactory.writeString(fieldName, (String) value);
+      } else if (value instanceof Boolean) {
+        pdxInstanceFactory.writeBoolean(fieldName, (Boolean) value);
+      } else if (value instanceof Integer) {
+        pdxInstanceFactory.writeInt(fieldName, (Integer) value);
+      } else if (value instanceof Byte) {
+        pdxInstanceFactory.writeByte(fieldName, (Byte) value);
+      } else if (value instanceof Long) {
+        pdxInstanceFactory.writeLong(fieldName, (Long) value);
+      } else if (value instanceof byte[]) {
+        pdxInstanceFactory.writeByteArray(fieldName, (byte[]) value);
+      } else if (value instanceof Double) {
+        pdxInstanceFactory.writeDouble(fieldName, (Double) value);
+      } else if (value instanceof PdxInstance) {
+        pdxInstanceFactory.writeObject(fieldName, value);
+      } else if (value instanceof List) {
+        pdxInstanceFactory.writeObject(fieldName, value);
+      } else if (value == null) {
+        pdxInstanceFactory.writeObject(fieldName, null);
+      } else {
+        throw new IllegalStateException(
+            "Don't know how to translate object of type " + value.getClass() + ": " + value);
+      }
+    }
+
+    return pdxInstanceFactory.create();
   }
 
   @Override
   public void init(Cache cache) {
-    structSerializer.init(cache);
-  }
-
-  private Struct typedStructToStruct(TypedStruct typedStruct)
-      throws IOException, ClassNotFoundException {
-    if (!serverTypeMap.containsKey(typedStruct.getTypeId())) {
-      throw new ClassNotFoundException("No matching type registered");
-    }
-    List<String> fieldNames = serverTypeMap.get(typedStruct.getTypeId());
-    if (fieldNames.size() != typedStruct.getFieldsCount()) {
-      throw new IOException("Incorrect number of fields for encoded type");
-    }
-    final Struct.Builder builder = Struct.newBuilder();
-    Iterator<String> keys = fieldNames.iterator();
-    Iterator<Value> values = typedStruct.getFieldsList().iterator();
-    while (keys.hasNext() && values.hasNext()) {
-      final Value nextVal = values.next();
-      if (nextVal.hasTypedStructValue()) {
-        builder.putFields(keys.next(), Value.newBuilder()
-            .setStructValue(typedStructToStruct(nextVal.getTypedStructValue())).build());
-      } else {
-        builder.putFields(keys.next(), nextVal);
-      }
-    }
-    return builder.build();
+    this.cache = cache;
   }
 }
